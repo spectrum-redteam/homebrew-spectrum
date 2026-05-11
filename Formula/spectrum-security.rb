664fad08441d42e707e7aa0ad3ca57769cec007e
William Jiang <jiangwilliam30@gmail.com>
2026-05-11 14:23:32 +0800

    Bump to 1.35.8

--- Formula/spectrum-security.rb
+++ Formula/spectrum-security.rb
@@ -2,8 +2,8 @@ class SpectrumSecurity < Formula
   include Language::Python::Virtualenv
   desc "Autonomous Cyber Reasoning System - Red Team & Blue Team AI agents"
   homepage "https://github.com/spectrum-redteamer/spectrum-project"
-  url "https://files.pythonhosted.org/packages/8a/22/fdba5f4d557e748ff817ade423d734b6304e04901c805b022b1d235699e4/spectrum_security-1.35.5.tar.gz"
-  sha256 "4192b44dfd79bea1694b9b5266ea64923789e2d57163f042ccbe877f3b9eeb29"
+  url "https://files.pythonhosted.org/packages/c0/cc/c5152bc9c4f7d56b70a0b171060b0fe16beb7e039bbfdf351048cbc15f70/spectrum_security-1.35.8.tar.gz"
+  sha256 "7a06efb87d915c847ef19cd1c446b4684a75c4a024931f901a3214941d4b79eb"
   license "MIT"
 
   depends_on "python@3.11"
