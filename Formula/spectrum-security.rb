734a0fc39bdfc5a3a7878cb45e34e07e3b0db721
William Jiang <jiangwilliam30@gmail.com>
2026-05-12 12:46:39 +0800

    Add spectrum-security formula

--- Formula/spectrum-security.rb
+++ Formula/spectrum-security.rb
@@ -1,9 +1,10 @@
 class SpectrumSecurity < Formula
   include Language::Python::Virtualenv
+
   desc "Autonomous Cyber Reasoning System - Red Team & Blue Team AI agents"
-  homepage "https://github.com/spectrum-redteamer/spectrum-project"
-  url "https://files.pythonhosted.org/packages/45/60/cef00a61140724b708a86cbe470fcc00ac0ec66e8ab6c492e26922757896/spectrum_security-1.35.17.tar.gz"
-  sha256 "5d3d2d85264b4417d9d78e8e632eb14baa93f183cdbf62dbac558760a3f8a1d7"
+  homepage "https://github.com/spectrum-redteam/spectrum"
+  url "https://files.pythonhosted.org/packages/77/fc/9da61fe5c4dc2826f94adbc598ad88d85ea23cc7d14be69cfb977a6d6484/spectrum_security-1.35.0.tar.gz"
+  sha256 "b6712893b7d656b17d71fc6c882bf1cef6e6ec55107d287d16bbfb4386a12ad3"
   license "MIT"
 
   depends_on "python@3.11"
